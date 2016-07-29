e2382909d8873c67abc5761d163acea71f356d5c
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-07-29 15:39:56 -0500

    initiation

---
+++ YJTaxonomer2/YRJ.hpp
@@ -0,0 +1,38 @@
+//
+//  YRJ.hpp
+//  building_vectors
+//
+//  Created by Younies Mahmoud on 7/15/16.
+//  Copyright © 2016 Younies Mahmoud. All rights reserved.
+//
+
+#ifndef YRJ_hpp
+#define YRJ_hpp
+#include "headers.h"
+#include <stdio.h>
+
+
+class YRJ {
+    LONG kmerLength;
+    LONG numOfKmers;
+    string path_to_file;
+    LONG * kmersVector;
+    
+    Node trie_node;
+    
+public:
+    YRJ(string path_to_file);
+    ~YRJ() ;
+    bool isKmerExist(LONG kmer);
+    //LONG getRandomSamples(vector<LONG> &samples , LONG numOfSamples);
+    vector<LONG> getRandomSamples(string file_path, LONGS numOfSamples);// this function free all the memory after getting the samples
+    
+    void set_trie_node(Node node);
+    
+    LONGS getTrie_Index();
+    LONGS get_kmers_numbers(){return this->numOfKmers;}
+    LONG get_kmer_index(LONGS index);
+    void Hash_the_kmers(LONG hash);
+};
+
+#endif /* YRJ_hpp */
---
+++ YJTaxonomer2/configuration.h
@@ -0,0 +1,54 @@
+//
+//  configuration.h
+//  YJTaxonomer
+//
+//  Created by Younies Mahmoud on 7/18/16.
+//  Copyright © 2016 Younies Mahmoud. All rights reserved.
+//
+
+#ifndef configuration_h
+#define configuration_h
+#include "headers.h"
+using namespace std;
+
+
+
+//variables
+
+string pattern = "##-#--###---#-#-#-#-#--#-##--##";
+LONG hash_pattern = 4378607365427089167;
+
+//variables_string
+
+string kmer_length_str = "31";
+
+LONGS random_number_size = 1000000;
+string path_to_random_numbers = "/export1/project/hondius/newKrakenResearch/generate_unique_random/MillionRandomIndices.txt";
+
+//names
+string name_of_the_databases = "databases";
+string name_of_output_results = "outputResults_newUIDs" +kmer_length_str + "_withKmers_withUIDS_with_levels.txt";
+
+string name_of_genome_classified = "the_classified_genome.txt";
+
+
+
+//names of the files
+
+string path_to_the_results = "/export1/project/hondius/newKrakenResearch/final_results/";
+string name_of_exact_output = "exact_output_file.txt";
+string name_of_in_exact_output = "in_exact_output_file.txt";
+string name_of_exact_hits = "exact_hits_file.txt";
+string name_of_in_exact_hits = "in_exact_hits_file.txt";
+
+//pathes
+string path_to_the_program_directory = "/export1/project/hondius/newKrakenResearch/";
+string path_to_the_genomes_database = "/export1/project/hondius/newKrakenResearch/databases/kmerDatabase_new_31/";
+string path_to_the_names_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/names.txt";
+string path_to_the_nodes_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/nodes.txt";
+string path_to_the_specific_nodes = "/export1/project/hondius/newKrakenResearch/databases/uids_new.txt";
+
+
+
+
+#endif /* configuration_h */
