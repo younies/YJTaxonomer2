3cb2b1f0ba2cea124b6b3c1fc7133e7a04464c72
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-07-29 16:08:19 -0500

    initiation2

---
+++ YJTaxonomer2/inexact_new_experiment.cpp
@@ -0,0 +1,21 @@
+//
+//  inexact_new_experiment.cpp
+//  YJTaxonomer2
+//
+//  Created by Younies Mahmoud on 7/29/16.
+//  Copyright © 2016 Younies Mahmoud. All rights reserved.
+//
+
+#include "configuration.h"
+#include "headers.h"
+#include "node_reader.hpp"
+#include "tree_builder.hpp"
+#include "YRJ.hpp"
+
+
+
+
+int main()
+{
+    
+}
